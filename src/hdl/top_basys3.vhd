650a5a3f1f17bc7ac7e54f551d700aedef3f28da
Yasci82 <141975504+Yasci82@users.noreply.github.com>
2024-04-15 18:22:43 -0600

    trying to see if the code is working because I was having issues with it

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -90,23 +90,104 @@ entity top_basys3 is
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
-  
+    component clock_divider is 
+        generic (constant k_DIV : natural := 2);
+        
+            port(i_clk : in std_logic;
+                 i_reset  : in std_logic;
+                 o_clk : out std_logic);
+                 
+    end component clock_divider;
 	-- declare components and signals
-
-  
+    signal w_fsm_reset : std_logic := '0';
+    signal w_clk_reset : std_logic := '0';
+    signal w_fsm_clk : std_logic := '0';
+    signal w_tdm_clk : std_logic := '0';
+    signal w_floor : std_logic_vector(3 downto 0) := "0010";
+    signal w_D3, w_D2, w_D1, w_D0, w_data : std_logic_vector (3 downto 0);
+    signal w_sel : std_logic_vector (3 downto 0);
+    
+    component sevenSegDecoder is
+        port ( i_D : in std_logic_vector (3 downto 0);
+               o_S : out std_logic_vector (6 downto 0));
+               
+    end component sevenSegDecoder;
+    
+    component elevator_controller_fsm is
+            Port ( i_clk      : in  STD_LOGIC;
+                   i_reset      : in  STD_LOGIC; -- synchronous
+                   i_stop      : in  STD_LOGIC;
+                   i_up_down : in  STD_LOGIC;
+                   o_floor      : out STD_LOGIC_VECTOR (3 downto 0));
+                   
+        end component elevator_controller_fsm;
+    
+    component TDM4 is
+               
+                -- fill in from TDM4.vhd
+              -- entity TDM4 is
+                    generic ( constant k_width : natural  := 4); -- bits in input and output
+                    Port ( i_clk    : in  STD_LOGIC;
+                           --i_reset  : in  STD_LOGIC; -- asynchronous
+                           i_D3     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+                           i_D2     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+                           i_D1     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+                           i_D0     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+                           o_data   : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+                           o_sel    : out STD_LOGIC_VECTOR (3 downto 0) -- selected data line (one-cold)
+                    );
+    end component TDM4;
+    
+    
 begin
 	-- PORT MAPS ----------------------------------------
-
+    SevenSegmentDisplayDecoder_uut: sevenSegDecoder
+        port map(
+                i_D => w_floor,
+                o_S => seg
+                 );
 	
-	
+	uut_inst : TDM4 
+         port map ( i_clk   => w_tdm_clk,
+                   -- i_reset => w_reset,     --w_fsm_reset
+                    i_D3    => w_D3,
+                    i_D2    => w_D2,
+                    i_D1    => w_D1,
+                    i_D0    => w_D0,
+                    o_data  => w_data,
+                    o_sel   => w_sel
+         );
+                     
+     elevator_inst : elevator_controller_fsm port map (
+             i_clk     => w_fsm_clk,
+             i_reset   => w_fsm_reset,
+             i_stop    => sw(0),
+             i_up_down => sw(1),
+             o_floor   => w_floor
+         );
+                                
+     clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
+         generic map ( k_DIV => 50000000 ) -- 1 Hz clock from 100 MHz
+         port map (             
+             i_clk   => clk,
+             i_reset => w_clk_reset,
+             o_clk   => w_fsm_clk
+         ); 
+         
 	-- CONCURRENT STATEMENTS ----------------------------
 	
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	
-
+	w_fsm_reset <= btnR or btnU;
+	w_clk_reset <= btnL or btnU;
+    
+    led(15) <= w_fsm_clk;
+    --led(14 downto 0) <= (others => '0');
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
+	an(2) <= '0';
 	
 	-- wire up active-low 7SD anodes (an) as required
 	-- Tie any unused anodes to power ('1') to keep them off
+	an(1 downto 0) <= (others => '1');
+	an(3) <= '1';
 	
 end top_basys3_arch;
